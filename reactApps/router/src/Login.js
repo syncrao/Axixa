104d654a41d33a5b008da38a83d36df3c0aa781a
raoshah <shahrukhrao1@gmail.com>
2025-05-16 08:30:47 +0530

    May 16

--- reactApps/router/src/Login.js
+++ reactApps/router/src/Login.js
@@ -1,5 +1,31 @@
+import { createContext, useContext } from 'react'
+const Legacy = createContext();
+
+const Baap = () => {
+    return (<>
+        <div>Baap</div>
+        <Beta/>
+        </>
+    )
+};
+
+const Beta = () => {
+    const val = useContext(Legacy)
+    return (
+        <div>Beta {val}</div>
+    )
+};
+
 function Login() {
-    return ( "login" );
+
+    return (
+        <Legacy.Provider value={12}>
+        <div >
+            dada
+        </div> 
+        <Baap/>
+        </Legacy.Provider>
+    )
 }
 
 export default Login;
